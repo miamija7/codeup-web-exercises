a1d754911594e05d1768610c5ef0b46b75976189
Mia Evans <miaevans@Mias-MBP.localdomain>
2022-07-27 09:41:55 -0500

    adding notes

---
+++ notes/coedup_java_notes/java_i/operators-exercise.js
@@ -0,0 +1,20 @@
+//OPERATORS EXCERCISE:
+
+'1' + 2;			                                //’12’
+typeof '1' + 2;			                            //‘string2’
+6%4;			                                    //2
+'3 + 4 is ' + 3 + 4;	                            //‘3 + 4 is 34‘
+0 < 0;				                                //false
+'false' == false;		                            //false
+true == 'true';			                            //false
+5 >= -5;			                                //true
+!false || false;			                        //true
+true || "42";			                            //true
+!true && !false;		                            //false
+6 % 5;				                                //1
+5 < 4 && 1 === 1;		                            //false
+typeof 'codeup' === 'string';                       //true
+'codeup' === 'codeup' && 'codeup' === 'Codeup';	    //false
+4 >= 0 && 1 !== '1';	                            //true
+6 % 3 === 0;		                                //true
+5 % 2 !== 0; 			                            //true
---
+++ notes/coedup_java_notes/java_i/primitive-types-exercise.js
@@ -0,0 +1,29 @@
+//PRIMITIVE TYPES EXCERCISE:
+
+99.9 		//‘number’
+"false" 	//‘string’
+false 		//'boolean'
+'0'		    //’string’
+0		    //‘number’
+true		//‘boolean’
+'true'		//‘string’
+
+/*
+Q: What type of data would best represent a perm or phrase typed into a search box?
+A: string
+
+Q: What type of data would best represent if a user is logged in?
+A: boolean
+
+Q: What type of data would best represent a discount amount to apply to a user’s shopping cart?
+A: number
+
+Q: What type of data would best represent whether or not a coupon code is valid?
+A: boolean
+
+Q: What type of data would best represent an email address typed into a registration form?
+A: string
+
+Q: What type of data would best represent the price of a product?
+A: number//
+*/
