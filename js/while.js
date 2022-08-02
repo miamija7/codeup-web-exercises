786ed9626f7e0a52c695537f67bae45757e2a533
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-08-02 14:59:48 -0500

    complete js loops

--- js/while.js
+++ js/while.js
@@ -1,12 +1,30 @@
 "use strict";
 console.log("========== while.js ===========");
-
-
-(function () {
-    let i = 1;
-    while (i < 65536) {
-        i *= 2;
-        console.log(i);
-    }
-
-})();
+//
+//
+// (function () {
+//
+//     let i = 1;
+//     while (i < 65536) {
+//         i *= 2;
+//         console.log(i);
+//     }
+//
+//     let totalCones = Math.floor(Math.random() * 51) + 50
+//     do {
+//         order();
+//     } while (totalCones !== 0)
+//     console.log("YAYYY! I sold them all!")
+//
+//
+//     function order(){
+//         let cones =  Math.floor(Math.random() * 5) + 1
+//         if (cones < totalCones) {
+//             console.log(`${cones} cones sold...`);
+//             totalCones -= cones;
+//         } else {
+//             console.log(`Cannot sell you ${cones} cones I only have ${totalCones}...`);
+//         }
+//     }
+//
+// })();
