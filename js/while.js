c751cef0794514665b6ce4953b17eecfa22c7b03
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-08-02 15:31:49 -0500

    js loops

--- js/while.js
+++ js/while.js
@@ -1,30 +1,33 @@
 "use strict";
 console.log("========== while.js ===========");
-//
-//
-// (function () {
-//
-//     let i = 1;
-//     while (i < 65536) {
-//         i *= 2;
-//         console.log(i);
-//     }
-//
-//     let totalCones = Math.floor(Math.random() * 51) + 50
-//     do {
-//         order();
-//     } while (totalCones !== 0)
-//     console.log("YAYYY! I sold them all!")
-//
-//
-//     function order(){
-//         let cones =  Math.floor(Math.random() * 5) + 1
-//         if (cones < totalCones) {
-//             console.log(`${cones} cones sold...`);
-//             totalCones -= cones;
-//         } else {
-//             console.log(`Cannot sell you ${cones} cones I only have ${totalCones}...`);
-//         }
-//     }
-//
-// })();
+
+let i = 1;
+while (i < 65536) {
+    i *= 2;
+    console.log(i);
+}
+
+let totalCones = Math.floor(Math.random() * 51) + 50
+
+do {
+    order();
+} while (totalCones > 0)
+
+console.log("Yay! I sold them all!")
+
+/*   ---------------------------------------------------------------
+ *  |                   Function: order()                           |
+ *  | Description: Customer orders 1 to 5 cones and subtracts       |
+ *  | from your totalCones count                                    |
+ *   ---------------------------------------------------------------
+*/
+function order(){
+    let cones =  Math.floor(Math.random() * 5) + 1
+    if (totalCones >= cones) {
+        console.log(`${cones} cones sold...`);
+        totalCones -= cones;
+    } else {
+        console.log(`Cannot sell you ${cones} cones I only have ${totalCones}...`);
+    }
+}
+
