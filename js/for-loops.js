8dcc0cfd5106028bd785792bd034c33b3618afe7
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-08-03 11:33:21 -0500

    update notes

--- js/for-loops.js
+++ js/for-loops.js
@@ -32,13 +32,17 @@ function isEven(number){
 
 // Exercise 4
 
-for (let i = 1; i < 10; i++){
-    let myString = ""
-    for(let j = 0; j < i; j++){
-        myString += i;
-    }
-    console.log(myString);
+for (let i = 1; i <= 9; i++){
+    console.log(i.toString().repeat(i));
 }
+
+// for (let i = 1; i < 10; i++){
+//     let myString = ""
+//     for(let j = 0; j < i; j++){
+//         myString += i;
+//     }
+//     console.log(myString);
+// }
 
 
 //Exercise 5
