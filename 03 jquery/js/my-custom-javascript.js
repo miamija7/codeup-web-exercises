39e14b1c1279dc0fb990df2749d9103ca5d8d5fa
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-08-25 15:08:37 -0500

    jquery mouse events

--- 03 jquery/js/my-custom-javascript.js
+++ 03 jquery/js/my-custom-javascript.js
@@ -11,5 +11,24 @@ $(function (){
     // $('li').css({'font-size':'20px'});
     // $('h1, p, li').css({'background-color': 'yellow'});
 
+
     // // EVENTS:
+    $('h1').click(function() {
+        $(this).css({'background-color':'teal'})
+    })
+
+    $('p').dblclick(function(){
+        $(this).css({'font-size':'18px'})
+    })
+
+    $('li').hover(function(){
+        $(this).css({'color':'red'})
+    }, function(){
+        $(this).css({'color':'black'})
+    })
+
+
+
+
+
 })
