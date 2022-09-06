e22f1aa6f2dec0fc3bb7dd1a1229f021cadb8bf8
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-06 09:49:23 -0500

    jquery FAQs add/hide function

--- 03 jquery/js/jquery_faq.js
+++ 03 jquery/js/jquery_faq.js
@@ -1,21 +1,30 @@
-$(function (){
-    // $('.toggle').on('click', function(e){
-    //     e.preventDefault();
-    //     console.log('toggle button clicked');
-    //     $('dd').toggleClass('invisible');
-    //     $('hr').toggleClass('invisible');
-    // })
-    //
+"use strict";
+
+$(function () {
+
+    let view = true;
+    $('.toggleView').on('click', function (e) {
+        e.preventDefault();
+        view = !view;
+        if (!view) {
+            $('dd').removeClass('invisible');
+            $(this).text("Hide All");
+        } else {
+            $('dd').addClass('invisible');
+            $(this).text("View All");
+        }
+    })
+
     // $('dt').on('click', function (){
     //     $(this).toggleClass('highlight');
     // })
 
-    $('.q-and-a').on('click', function(e){
+    $('.q-and-a').on('click', function (e) {
         e.preventDefault();
         $(this).children("dd").toggleClass('invisible');
     })
 
-    $('#national-parks').children().on('click', function(){
+    $('#national-parks').children().on('click', function () {
         $(this).toggleClass('bold');
     })
 });
