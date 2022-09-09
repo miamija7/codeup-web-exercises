6890a7f80665ee2ee0cd13774f3c02ac016ac239
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-09 09:23:22 -0500

    ajax store json load

--- 03 jquery/js/ajax-store.js
+++ 03 jquery/js/ajax-store.js
@@ -2,47 +2,45 @@
 
 // ADD JSON DATA TO PAGE
 const localUrl = "../data/inventory.json";
+let counter = 0;
 
-const loadJSON = ()=> {
+const loadJSON = () => {
     $.get(localUrl).done(function (data) {
-        data.forEach(item => {
-
-            $('#insertProducts').append(
-                `<tr>
+        if (counter < data.length) {
+            data.forEach(item => {
+                counter++;
+                $('#insertProducts').append(
+                    `<tr>
                     <td>${item.title}</td>
                     <td>${item.quantity}</td>
                     <td>${item.price}</td>
                     <td>${item.categories}</td>
-                </tr>`
-            )
-        })
+                    </tr>`
+                )
+            })
+        }
     })
 };
 
-$('#refresh').on('click', function(e){
+$('#refresh').on('click', function (e) {
     e.preventDefault();
     loadJSON();
 })
 
 
 
-const addItem = (name, qty, price, catArray)=> {
-
-}
-
-
-
 // PUSH NEW ITEM TO LOCAL ARRAY
-// localData.push(
-//     {
-//         title: "Camera",
-//         quantity: 7,
-//         price: 170.00,
-//         categories: [
-//             "technology",
-//             "tools",]
-//     },
-// );
+let localData = [];
+localData.push(
+    {
+        title: "Camera",
+        quantity: 7,
+        price: 170.00,
+        categories: [
+            "technology",
+            "tools",]
+    },
+);
 
 // APPEND ARRAY DATA TO TABLE
 const refreshJsonBtn = document.querySelector('#refresh');
