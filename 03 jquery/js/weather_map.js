8dc1e61b1163c82efa1b536697602aac783f91cc
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-13 12:12:31 -0500

    weather api

--- 03 jquery/js/weather_map.js
+++ 03 jquery/js/weather_map.js
@@ -1,13 +1,22 @@
-// WEATHER MAP B&W WIDGET
+//-------------- VARIABLES --------------
 
+
+const queries = [
+    {location: [-96.797, 32.777]},
+]
+
+//-------------- WEATHER MAP --------------
+
+// Weather Widget
 window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
 window.myWidgetParam.push({
-    id: 22,
-    cityid: '4684904',
+    id: 21,
+    cityid: '2643743',
     appid: 'bdd7146a61152b7ff3662650d34c0fb7',
     units: 'imperial',
-    containerid: 'openweathermap-widget-22',
+    containerid: 'openweathermap-widget-21',
 });
+
 (function () {
     var script = document.createElement('script');
     script.async = true;
@@ -16,3 +25,40 @@ window.myWidgetParam.push({
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(script, s);
 })();
+
+
+//-------------- MAPBOX --------------
+
+// Mapbox Map
+mapboxgl.accessToken = MAPBOX_KEY;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/dark-v10', // style URL
+    center: [-96.777, 32.777], // starting position [lng, lat]
+    zoom: 10, // starting zoom
+    projection: 'globe' // display the map as a 3D globe
+});
+
+// Mapbox Search
+map.addControl(
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    })
+);
+
+// Mapbox Markers
+const createMarker = (query) => {
+    const marker = new mapboxgl.Marker({
+        color: "lightskyblue",
+        draggable: true
+    }).setLngLat(query.location)
+        .addTo(map);
+}
+
+
+//-------------- FUNCTIONALITY --------------
+
+queries.forEach(query => {
+    createMarker(query);
+})
