5b8bd656963c21a683954a6944051fe534671323
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-13 13:43:42 -0500

    weather api widget

--- 03 jquery/js/weather_map.js
+++ 03 jquery/js/weather_map.js
@@ -2,29 +2,32 @@
 
 
 const queries = [
-    {location: [-96.797, 32.777]},
+    {
+        id: '4684888',
+        location: [-96.797, 32.777]
+    },
 ]
 
 //-------------- WEATHER MAP --------------
 
 // Weather Widget
-window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
-window.myWidgetParam.push({
-    id: 21,
-    cityid: '2643743',
-    appid: OPEN_WEATHER_KEY,
-    units: 'imperial',
-    containerid: 'openweathermap-widget-21',
-});
+const updateWidget = (query) => {
+    window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
 
-(function () {
+    window.myWidgetParam.push({
+        id: 21,
+        cityid: query.id,
+        appid: OPEN_WEATHER_KEY,
+        units: 'imperial',
+        containerid: 'openweathermap-widget-21',
+    })
     var script = document.createElement('script');
     script.async = true;
     script.charset = "utf-8";
     script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(script, s);
-})();
+}
 
 
 //-------------- MAPBOX --------------
@@ -54,17 +57,27 @@ const marker = new mapboxgl.Marker({
 }).setLngLat(queries[0].location)
     .addTo(map);
 
-marker.on('dragend', (e)=> {
+marker.on('dragend', (e) => {
     console.log(e);
     console.log(e.target._lngLat.lng, e.target._lngLat.lat);
-    queries.unshift({ location: [e.target._lngLat.lng, e.target._lngLat.lat]});
+    queries.unshift({location: [e.target._lngLat.lng, e.target._lngLat.lat]});
     getGeoCode(queries[0]);
 })
 
 //-------------- FUNCTIONALITY --------------
 
-const getGeoCode = async (query)=>{
-    const res = await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${query.location[1]}&lon=${query.location[0]}&limit=5&appid=bdd7146a61152b7ff3662650d34c0fb7`);
-    const data = res.json();
-    console.log(data);
+const getGeoCode = async (query) => {
+    try {
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${query.location[1]}&lon=${query.location[0]}&appid=${OPEN_WEATHER_KEY}`);
+        const data = await res.json();
+        console.log(data);
+        console.log(data.id.toString());
+        queries[0].id = data.id.toString();
+        updateWidget(queries[0]);
+    } catch (e) {
+        console.log('ERROR:', e);
+    }
 }
+
+// `http://api.openweathermap.org/geo/1.0/reverse?lat=${query.location[1]}&lon=${query.location[0]}&limit=5&appid=${OPEN_WEATHER_KEY}`
+// `https://api.openweathermap.org/data/2.5/weather?lat=${query.location[1]}&lon=${query.location[0]}&appid=${OPEN_WEATHER_KEY}`
