c9150e7e1eafc0edf43f61e8f8bc57a39ed06e56
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-14 11:25:57 -0500

    weather api

--- 03 jquery/js/weather_map.js
+++ 03 jquery/js/weather_map.js
@@ -1,83 +1,87 @@
-//-------------- PIN --------------
+"use strict";
+(()=> {
+    //-------------- MY PIN --------------
+    let pinInfo = { id: '4684888', location: [-96.797, 32.777] };
+    const appendWidget = document.querySelector('.appendWidget');
 
-let pinInfo = { id: '4684888', location: [-96.797, 32.777] }
 
-//-------------- WEATHER MAP --------------
+//-------------- MAPBOX API --------------
 
-const appendWidget = document.querySelector('.appendWidget');
-// Weather Widget
-const updateWidget = (pin) => {
-    const script = document.createElement('script');
-    script.async = true;
-    script.charset = "utf-8";
-    script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
-    const s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(script, s);
-
-    window.myWidgetParam = [];
-    appendWidget.innerHTML = `<div id="openweathermap-widget-21"></div>`;
-    window.myWidgetParam.push({
-        id: 21,
-        cityid: pin.id,
-        appid: OPEN_WEATHER_KEY,
-        units: 'imperial',
-        containerid: 'openweathermap-widget-21',
+// MAP
+    mapboxgl.accessToken = MAPBOX_KEY;
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/dark-v10', // style URL
+        center: [-96.777, 32.777], // starting position [lng, lat]
+        zoom: 10, // starting zoom
+        projection: 'globe'  // display the map as a 3D globe
     });
-}
+
+// MAP BACKGROUND
+    map.on('style.load', () => {
+        map.setFog({});
+    });
+
+// MAP SEARCH
+    const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    })
+    map.addControl(geocoder);
+
+// MAP MARKER
+    const marker = new mapboxgl.Marker({
+        color: "lightskyblue",
+        draggable: true
+    }).setLngLat(pinInfo.location).addTo(map);
 
 
-//-------------- MAPBOX --------------
+//-------------- WEATHER MAP API --------------
 
-// Mapbox Map
-mapboxgl.accessToken = MAPBOX_KEY;
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/dark-v10', // style URL
-    center: [-96.777, 32.777], // starting position [lng, lat]
-    zoom: 10, // starting zoom
-    projection: 'globe'  // display the map as a 3D globe
-});
+// FUNCTION: CREATE/REFRESH WEATHER WIDGET
+// PROTOTYPE: updateWidget();
+    const updateWidget = (pin) => {
+        const script = document.createElement('script');
+        script.async = true;
+        script.charset = "utf-8";
+        script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
+        const s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(script, s);
 
-// Mapbox Map Background
-map.on('style.load', () => {
-    map.setFog({});
-});
-
-// Mapbox Search
-const geocoder = new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken,
-    mapboxgl: mapboxgl
-})
-map.addControl(geocoder);
-
-
-// Mapbox Markers
-const marker = new mapboxgl.Marker({
-    color: "lightskyblue",
-    draggable: true
-}).setLngLat(pinInfo.location)
-    .addTo(map);
-
-
-//-------------- FUNCTIONALITY --------------
-
-const getGeoCode = async (pin) => {
-    try {
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${pin.location[1]}&lon=${pin.location[0]}&appid=${OPEN_WEATHER_KEY}`);
-        const data = await res.json();
-        pin.id = data.id.toString();
-    } catch (e) {
-        console.log('ERROR:', e);
+        window.myWidgetParam = [];
+        appendWidget.innerHTML = `<div id="openweathermap-widget-21"></div>`;
+        window.myWidgetParam.push({
+            id: 21,
+            cityid: pin.id,
+            appid: OPEN_WEATHER_KEY,
+            units: 'imperial',
+            containerid: 'openweathermap-widget-21',
+        });
     }
-}
 
-// EVENT LISTENER
-marker.on('dragend', async (e) => {
-    try {
-        pinInfo.location = [e.target._lngLat.lng, e.target._lngLat.lat];
-        await getGeoCode(pinInfo);
-        await updateWidget(pinInfo);
-    } catch (e) {
-        console.log(e);
+//-------------- MY CUSTOM JS --------------
+
+// EVENT LISTENER: MAP MARKER
+    marker.on('dragend', async (e) => {
+        try {
+            pinInfo.location = [e.target._lngLat.lng, e.target._lngLat.lat];    // reassign pin lat/lon
+            await getGeoCode(pinInfo);                                          // reassign pin city id
+            await updateWidget(pinInfo);                                        // refresh widget with lat/lon
+        } catch (e) {
+            console.log(e);
+        }
+    })
+
+// FUNCTION: GET CITY ID (for widget use)
+// PROTOTYPE: getGeoCode(pin);
+    const getGeoCode = async (pin) => {
+        try {
+            const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${pin.location[1]}&lon=${pin.location[0]}&appid=${OPEN_WEATHER_KEY}`);
+            const data = await res.json();
+            pin.id = data.id.toString();
+        } catch (e) {
+            console.log('ERROR:', e);
+        }
     }
-})
+
+})();
