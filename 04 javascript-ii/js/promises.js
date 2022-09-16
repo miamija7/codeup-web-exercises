acf5fc994777416755c7a29ae241e1958be43379
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-16 17:48:23 -0500

    github api

--- 04 javascript-ii/js/promises.js
+++ 04 javascript-ii/js/promises.js
@@ -2,5 +2,16 @@
 
 (() => {
     console.log("============ Exercise: Promises ============");
+    // fetch(url, {headers: {'Authorization': 'token YOUR_TOKEN_HERE'}})
 
+    const handleApiPromise = async (username) => {
+        try{
+            let res = await fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': GITHUB_KEY}});
+            let data = await res.json();
+            return data[0];
+        } catch (e) {
+            console.log("ERROR: ", e);
+        }
+    };
+    console.log('Handle An API Promise Results:', handleApiPromise("miamija7"));
 })();
