a9c92f279bdc2f0e60cf4abdb9b4ffa8dd2de997
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-09-16 15:21:03 -0500

    js reduce

--- 04 javascript-ii/js/map-filter-reduce.js
+++ 04 javascript-ii/js/map-filter-reduce.js
@@ -44,21 +44,50 @@
     const experiencedUsers = users.filter(user => user.languages.length >= 3);
     console.log("TODO 1 check:\n", experiencedUsers);
 
+
     //Todo: Use .map to create an array of strings where each element is a user's email address
-    const emails = users.map(({ email }) => email);
+    const emails = users.map(({email}) => email);
     console.log("TODO 2 check:\n", emails);
 
+
     //Todo: Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
-    const totalYearsOfExperience = users.reduce((accumulator, user) => {
-        return Number(accumulator) + Number(user.yearsOfExperience) }, 0);
-    const avgYearsOfExperience = totalYearsOfExperience/users.length;
-    console.log("TODO 3 check:\n", totalYearsOfExperience);
-    console.log("TODO 3 check:\n", avgYearsOfExperience);
+    const avgYearsOfExperience = () => {
+        const sumYears = users.reduce((accumulator, user) => {
+            return Number(accumulator) + Number(user.yearsOfExperience)
+        }, 0);
+        return sumYears / users.length;
+    }
+    console.log("TODO 3 check:\n", avgYearsOfExperience());
+
 
     //Todo: Use .reduce to get the longest email from the list of users
-    const longestEmail = users.reduce((longest, user) => {
-        return (longest.length < user.email.length) ? user.email : longest;
+    const longestEmail = users.reduce((prev, curr) => {
+        return (prev.length < curr.email.length) ? curr.email : prev;
     }, users[0].email);
-    console.log(longestEmail);
+    console.log("TODO 4 check:\n", longestEmail);
+
+
+    //Todo: Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+    const names = users.reduce((accumulator, curr) => {
+        if (curr !== users[users.length - 1])
+            return accumulator += curr.name + ", ";
+        else
+            return accumulator += curr.name + ".";
+
+    }, 'Your instructors are: ');
+    console.log("TODO 5 check:\n",names);
+
+    //Todo: BONUS: Use .reduce to get the unique list of languages from the list of users.
+    const languages =  users.reduce((previousValue, currentValue) => {
+            return [...previousValue, ...currentValue.languages];
+        },[]
+    );
+
+    const uniqueLanguages = languages.reduce((prev, curr) => {
+            if (!prev.includes(curr)) return [...prev, curr];
+            else return [...prev];
+        },[]
+    );
+    console.log("TODO 6 check:\n", languages, uniqueLanguages)
 
 })();
